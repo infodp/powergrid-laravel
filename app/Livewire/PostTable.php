d4fde06f701a126ce4349a4d19865403f7abda3b
infodp <diprimiogustavo@gmail.com>
2024-09-02 15:46:14 -0300

    tables with powergrid

---
+++ app/Livewire/PostTable.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Log;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Exportable;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+use PowerComponents\LivewirePowerGrid\Footer;
+use PowerComponents\LivewirePowerGrid\Header;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\WithExport;
+
+final class PostTable extends PowerGridComponent
+{
+    use WithExport;
+
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        return Post::query();
+    }
+
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id')
+            ->add('title')
+            ->add('content')
+            ->add('status')
+            ->add('created_at');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Id', 'id'),
+            Column::make('Title', 'title')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Content', 'content')
+                ->sortable()
+                ->searchable()
+                ->editOnClick(hasPermission:true),
+
+            Column::make('Status', 'status')
+                ->sortable()
+                ->searchable()
+                ->toggleable(hasPermission:true),
+
+            Column::action('Action')
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+        ];
+    }
+
+    #[\Livewire\Attributes\On('edit')]
+    public function edit($rowId): void
+    {
+        $this->js('alert('.$rowId.')');
+    }
+
+    public function actions(Post $row): array
+    {
+        return [
+            Button::add('edit')
+                ->slot('Edit: '.$row->id)
+                ->id()
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
+                ->dispatch('edit', ['rowId' => $row->id])
+        ];
+    }
+
+    public function onUpdatedEditable(string|int $id, string $field, string $value): void
+    {
+        Post::query()->find($id)->update([
+            $field => $value
+        ]);
+    }
+
+    public function onUpdatedToggleable(string $id, string $field, string $value): void
+    {
+        Log::info("Actualizamos el campo $field con el valor $value para el ID $id");
+        Post::query()->find($id)->update([
+            $field => $value
+        ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Laravel</title>
+
+        <style>
+            body {
+                margin-top: 10px;
+                background: #111827;
+            }
+        </style>
+    </head>
+    <body>
+        <livewire:post-table/>
+        @livewireScripts
+        @vite('resources/js/app.js')
+    </body>
+</html>
